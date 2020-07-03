cfeca98f4171600f69c0d4c2caffe789712b2737
SanchariGautam14 <sanchari_g@thirdeyedata.io>
2020-07-03 13:57:55 +0000

    Conflict fix

--- FrontEnd/js/config.js
+++ FrontEnd/js/config.js
@@ -1,9 +1,4 @@
 var environments ={
-<<<<<<< Updated upstream
-     apiBackendBaseUrl : "https://15d8fb5c6e7f.ngrok.io/recommendation-engine",
-     imageBaseUrl : "https://15d8fb5c6e7f.ngrok.io/media/"
-=======
      apiBackendBaseUrl : "http://apilaiout.thirdeyedata.io/recommendation-engine",
      imageBaseUrl : "http://apilaiout.thirdeyedata.io/media/"
->>>>>>> Stashed changes
 }
