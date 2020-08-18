081083afc53a8cfc354eba77a4903c964775e1a6
Sanchari Gautam <sanchari_g@thirdeyedata.io>
2020-08-18 17:39:04 +0530

    Fine tune text-placement

--- recommendationEngine/Utils/gan_postprocess.py
+++ recommendationEngine/Utils/gan_postprocess.py
@@ -3,14 +3,17 @@ import os
 import pandas as pd
 import numpy as np
 import math
+from scipy.spatial import distance
 
 def get_tag(center, tag_file):
     dist, color = [], []
     for i in range(len(tag_file)):
         c = tag_file.loc[i,['B','G','R']]
-        dist.append(math.sqrt(sum([(a - b) ** 2 for a, b in zip(center, c)])))
+        # dist.append(math.sqrt(sum([(a - b) ** 2 for a, b in zip(center, c)])))
+        dist.append(distance.euclidean(center, c))
     min_i = dist.index(min(dist))
     tag = tag_file['Floor tags'].iloc[min_i]
+    # print(tag)
     return tag
 
 
@@ -46,14 +49,18 @@ def place_text(img,clust, centers, tag_file,length,width,bedrooms):
     i = 1
     legends = []
     areas = [cv2.contourArea(c) for c in contours if cv2.contourArea(c) < 1000000]
-    # max_contarea = max(areas)
-    # print(type(areas))
-    # print(max_contarea)
+    
     max_contarea=sum(areas)
     total_area=int(length)*int(width)
-    # print(totarea)
-    tagsli=[]
+    areali=[]
+    a=[]
+    puttextlabels=[]
+    tags=[]
+    centroids=[]
     bedcount=0
+    tagli=[]
+    numli=[]
+    cen=[]
     for c in contours:
         cont_area = cv2.contourArea(c)
         if cont_area < 100:
@@ -62,42 +69,101 @@ def place_text(img,clust, centers, tag_file,length,width,bedrooms):
             continue
         else:
             grain = np.int0(cv2.boxPoints(cv2.minAreaRect(c))) 
-            centroid =( grain[2][0]-(grain[2][0]-grain[0][0])//2, grain[2][1]-(grain[2][1]-grain[0][1])//2) 
+            centroid =( grain[2][0]-(grain[2][0]-grain[0][0])//3, grain[2][1]-(grain[2][1]-grain[0][1])//3) 
             color = clust[centroid]
             color_list = color.tolist()
             leg={}
+            
             min_rect = cv2.minAreaRect(c)
-            if color_list in centers.tolist() and min(min_rect[1][1], min_rect[1][0]) > 20:
+            if color_list in centers.tolist() and min(min_rect[1][1], min_rect[1][0]) > 10:
                 area_perc=round((cont_area/max_contarea)*100, 2)
                 tag = get_tag(color, tag_file)
-                # cv2.putText(img, str(i),  centroid, cv2.FONT_HERSHEY_SIMPLEX,1.5, (0, 0, 0),thickness=3)
                 leg['room']=str(i)
                 leg['area_perc']=tag+"-"+str(area_perc)
-                # leg['area_perc']={'tag': tag, 'area': round((cont_area/max_contarea)*100, 2) }
-                print(leg)
-                # print(tag)
-                if (tag=='stair' and tag not in tagsli):
-                    # print("inside if")
-                    cv2.putText(img, str(i),  centroid, cv2.FONT_HERSHEY_SIMPLEX,1.5, (0, 0, 0),thickness=3)
-                    legends.append(leg)
-                    tagsli.append(tag)
-                else:
-                    if tag!='stair':
-                        # print("inside else")
-                        if area_perc>5:
-                            if tag=="bedroom":
-                                bedcount+=1
-                                if bedcount<=bedrooms:
-                                    cv2.putText(img, str(i),  centroid, cv2.FONT_HERSHEY_SIMPLEX,1.5, (0, 0, 0),thickness=3)
-                                    legends.append(leg)
+                if tag!='stair':
+                    if area_perc>3:
+                        
+                        if tag=="bedroom":
+                            bedcount+=1
+                            if bedcount<=bedrooms:
+                                puttextlabels.append({str(i):[centroid,tag,area_perc]})
+                                tags.append(tag)
+                        else:
+                            if tag not in tags:
+                                if tag!='clos':
+                                    puttextlabels.append({str(i):[centroid,tag,area_perc]})
+                                    tags.append(tag)
+                                else:
+                                    areali.append(area_perc)
+                                    centroids.append(centroid)
                             else:
-                                cv2.putText(img, str(i),  centroid, cv2.FONT_HERSHEY_SIMPLEX,1.5, (0, 0, 0),thickness=3)
-                                legends.append(leg)
-                # print(tagsli)
-                # legends.update({"room":str(i), "area_perc": str({'tag': tag, 'area': round((cont_area/max_contarea)*100, 2) })})
-                i+=1
-    print(legends)
-    return img, legends
+                                areali.append(area_perc)
+                                centroids.append(centroid)
+                    i+=1 
+                else:
+                    areali.append(area_perc)
+                    centroids.append(centroid)  
+                
+            else:
+                area_perc=round((cont_area/max_contarea)*100, 2)
+                areali.append(area_perc)
+                centroids.append(centroid)  
+                
+    desc_area=areali
+    desc_area.sort(reverse=True)
+    if "living/ dining" in tags :
+        roomnames=['kitchen','bath']
+    elif  "kitchen/ dining" in tags:
+        roomnames=['living','dining','bath']
+    else:
+        roomnames=['living','dining','kitchen','bath']
+    tot_rooms=len(roomnames)+bedrooms
+    for j in range(0,len(roomnames)):
+        if (roomnames[j] not in tags) and (j<len(desc_area)):
+            i+=1
+            tags.append(roomnames[j])
+            puttextlabels.append({str(i):[centroids[areali.index(desc_area[j])],roomnames[j],desc_area[j]]})
+
+    while (bedrooms-bedcount)!=0:
+        if j <len(desc_area):
+            tags.append("bedroom")
+            puttextlabels.append({str(i):[centroids[areali.index(desc_area[j])],"bedroom",desc_area[j]]})
+            bedcount+=1
+            j+=1
+        else:
+            break
+    area=[]
+    for p in puttextlabels:
+        area.append(list(p.values())[0][2])
+    area.sort(reverse=True)
+    final_legends=[]
+    for a in area:
+        for p in puttextlabels:
+            if list(p.values())[0][2] == a:
+                name=str(list(p.values())[0][1])
+                areaperc=list(p.values())[0][2]
+                centroid=list(p.values())[0][0]
+                cv2.putText(img, list(p.keys())[0],  centroid, cv2.FONT_HERSHEY_SIMPLEX,1.5, (109, 111, 115),thickness=3)
+                  
+                final_legends.append({"room":list(p.keys())[0],"area_perc":name+"-"+str(areaperc)})
+
+    used_area_perc=0
+    for l in final_legends:
+        used_area_perc+=float(l['area_perc'].split('-')[-1])
+    unplanned_openspace=100-used_area_perc
+    leg={}
+    i+=1
+    leg['room']=str(i)
+    leg['area_perc']="open_space"+"-"+str(unplanned_openspace)
+    final_legends.append(leg)
+    leg={}
+    i+=1
+    leg['room']=str(i)
+    leg['area_perc']="planned_floor_area"+"-"+str(used_area_perc)
+    final_legends.append(leg)
+    print(puttextlabels)
+    print(final_legends)
+    return img, final_legends
 
 
 def gan_convert(img, tag_file,length,width,bedrooms):
@@ -106,5 +172,3 @@ def gan_convert(img, tag_file,length,width,bedrooms):
     final_img, legends = place_text(blkwht_img, cluster_img, centers, tag_file,length,width,bedrooms)
     return final_img, legends
 
-# def get_bedroom_pos(image_pth):
-#     im=cv2.imread(image_pth)
